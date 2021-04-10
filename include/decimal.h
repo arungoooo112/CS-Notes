718271e7dd6ece4d6296903b5a5f3ee781e94858
W_Frank <61907503+arungoooo112@users.noreply.github.com>
2021-04-10 16:45:56 +0800

    add decimal.h

---
+++ include/decimal.h
@@ -0,0 +1,31 @@
+#include <string>
+#include <algorithm>
+
+std::string ten2n(int dec, int n = 10);
+int n2ten(std::string nec, int n = 10); 
+// stoi(nec, nullptr, n);
+
+std::string ten2n(int dec, int n = 10) 
+{
+	std::string ans="";
+	do{
+		int t=dec%n;
+		if(t>=0&&t<=9)	ans+=t+'0';
+		else ans+=t-10+'a';
+		dec/=n;
+	}while(dec!=0);	//使用do{}while（）以防止输入为0的情况
+	std::reverse(ans.begin(),ans.end());
+	return ans;	
+}
+
+int n2ten(std::string nec, int n = 10) 
+{
+	int ans=0;
+	for(int i=0;i<nec.size();i++)
+	{
+		char t=nec[i];
+		if(t>='0'&&t<='9') ans=ans*n+t-'0';
+		else ans=ans*n+t-'a'+10;
+	}
+	return ans;
+}
